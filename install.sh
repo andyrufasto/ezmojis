66556591a817692c578ffb5ecff719fb0af013eb
Andy Rufasto <andy@andyrufasto.cf>
2021-08-10 14:19:06 -0500

    remove new line

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 PATH_A=${HOME}"/.local/bin"
 DATA_HOME_A=${HOME}"/.local/share/ezmojis"
 mkdir ${DATA_HOME_A}
-cp ezmojis ${PATH_A}
-cp emojis.txt devicons.txt ${DATA_HOME_A}
+cp src/ezmojis ${PATH_A}
+cp src/emojis.txt src/devicons.txt ${DATA_HOME_A}
 
